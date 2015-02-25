c48f5c2a8b60771543b7451a4f3df87caee00908
Mark Mark <makroo@yandex.ru>
2015-02-25 14:53:57 +0200

    Add DBManager, remove storyboard

---
+++ Weather/Weather/DBManager.h
@@ -0,0 +1,23 @@
+//
+//  DBManager.h
+//  Weather
+//
+//  Created by Mark Prutskiy on 25.02.15.
+//  Copyright (c) 2015 Mark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface DBManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++ (DBManager *)sharedInstance;
+
+@end
