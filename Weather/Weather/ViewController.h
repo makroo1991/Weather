f5ffb4837b6b31c731344726e4be259b29c9406a
Mark Mark <makroo@yandex.ru>
2015-02-25 14:36:35 +0200

    add empty project

---
+++ Weather/Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Mark Prutskiy on 25.02.15.
+//  Copyright (c) 2015 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
